7ba266a2a78e790be9dedc8c7dbd7b159369a961
Kathy Lin <katherine.m.lin@gmail.com>
2017-04-01 13:11:41 -0700

    end of chapter one commit

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // outlets are a reference to an object.
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    // arrays are delcared using let
+    // a constant is denoted with let
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
+    // variables are declared with var, their values are allowed to change
+    var currentQuestionIndex: Int = 0;
+    
+    // an action method. when a UIButton is tapped, it calls a method on another object (the "target")
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        
+        // if we are at the last question, go back to the beginning
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0;
+        }
+        
+        let question: String = questions[currentQuestionIndex];
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    // overriding a method
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
